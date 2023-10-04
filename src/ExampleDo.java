c75890be67a76ae2e56689626ad36b35b44a96b6
AlexTouchedGrass <dean-potts.622237@swl.k12.oh.us>
2023-10-04 09:39:55 -0400

    No way was this that hard

--- src/ExampleDo.java
+++ src/ExampleDo.java
@@ -1,5 +1,29 @@
-public class Main {
+import java.util.Scanner;
+
+public class ExampleDo {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scan = new Scanner(System.in);
+
+        String name1 = "Alex";
+        String name2 = "Gorg";
+        String name3 = "Bob";
+
+        int ageAlex = 17;
+        int ageGorg = 29;
+        int ageBob = 69;
+
+        String colorAlex = "Red";
+        String colorGorg = "Red";
+        String colorBob = "Blue";
+
+        System.out.printf("%-10s%5s%7s" , "Name" , "Age" , "Color");
+        System.out.printf("\n%-10s%5d%10s", name1, ageAlex, colorAlex);
+        System.out.printf("\n%-10s%5d%10s", name2, ageGorg, colorGorg);
+        System.out.printf("\n%-10s%5d%10s", name3, ageBob, colorBob);
+
+
+
+
+
     }
 }
